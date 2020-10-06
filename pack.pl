53590ad6bf5fbb5eecbfd8951de6fbaea0db0151
Falco Nogatz <fnogatz@gmail.com>
2020-10-06 16:39:52 +0200

    Add pack.pl

---
+++ pack.pl
@@ -0,0 +1,12 @@
+name(dcg_visualiser).
+
+version('1.0.0-beta.0').
+download('https://github.com/fnogatz/dcg-visualiser/releases/*.zip').
+
+title('Web-based visualisation for DCGs').
+author('Falco Nogatz','fnogatz@gmail.com').
+author('Jona Kalkus','jonakalkus@gmail.com').
+maintainer('Falco Nogatz','fnogatz@gmail.com').
+packager('Falco Nogatz','fnogatz@gmail.com').
+
+home('https://github.com/fnogatz/dcg-visualiser').
